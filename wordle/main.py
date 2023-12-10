172a2da606c6b96cb66fe5cba2bc6e81ec79d9c2
merkol <merterkol@outlook.com>
2023-12-11 00:58:02 +0300

    updates

--- wordle/main.py
+++ wordle/main.py
@@ -16,20 +16,22 @@ pygame.display.set_caption("Hexagon Word Guessing Game")
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
-button_rect = pygame.Rect(WIDTH - 150, 100, 100, 50)  # Button position and size
+button_rect = pygame.Rect(WIDTH - 150, 100, 125, 50)  # Button position and size
+letter_rect = pygame.Rect(WIDTH - 150, 200, 100, 50)  # Letter position and size
 
-# List of words
-words = ['apple', 'banana', 'orange', 'grape', 'watermelon']  # Add more words as needed
-description = ['a fruit', 'a fruit', 'a fruit', 'a fruit', 'a fruit']  # Add more descriptions as needed# Add more indexes as needed
+
+# Create a dictionary of words and descriptions
+words = {"apple": "elma", "banana": "muz", "orange": "portakal", "grape": "uzum", "watermelon": "karpuz"}
+
 
 def random_word(words):
-    chosen_word = random.choice(words)
+    chosen_word = random.choice(list(words.keys()))
     word_length = len(chosen_word)
-    word_description = description[words.index(chosen_word)]
+    word_description = words[chosen_word]
     indexes = [i for i in range(len(chosen_word))]
     point_of_word = 100 * len(chosen_word)
     guessed_letters = ['_'] * word_length
-    words.remove(chosen_word)
+    words.pop(chosen_word)
     return chosen_word, word_length, word_description, indexes, point_of_word, guessed_letters
 
 def reveal_random_letter(chosen_word, indexes, guessed_letters):
@@ -71,16 +73,25 @@ async def main():
     paused_time = 0
     global timer_running, active, guess, chosen_word, word_length, word_description, indexes, point_of_word, guessed_letters, total_point,running
     while running:
+        
+        if not timer_running:
+            ## quit game if paused time is more than 15 seconds
+            if pygame.time.get_ticks() - paused_start_time > 15000:
+                ## display a message
+                running = False
+                break
+            
         win.fill(WHITE)  # Fill the window with white color    
         await asyncio.sleep(0)
+        
         # Display total point
         font = pygame.font.SysFont(None, 36)
-        text = font.render('Total points: ' + str(total_point), True, BLACK)
+        text = font.render('Toplam puan: ' + str(total_point), True, BLACK)
         win.blit(text, (50, 20))
 
         # Display remaining word point
         font = pygame.font.SysFont(None, 36)
-        text = font.render('Remaining word point: ' + str(point_of_word), True, BLACK)
+        text = font.render('Kalan Harf Puanı: ' + str(point_of_word), True, BLACK)
         win.blit(text, (50, 50))
         
         # Draw hexagon grid for guessed letters
@@ -91,32 +102,39 @@ async def main():
             draw_hexagon(offset_x + i * (hex_width + 10), offset_y)
         
         font = pygame.font.SysFont(None, 28)
-        text = font.render('Description: ' + word_description, True, BLACK)
+        text = font.render('Açıklama: ' + word_description, True, BLACK)
         text_rect = text.get_rect(midtop=(WIDTH // 2, offset_y + hex_height + 20))
         win.blit(text, text_rect)
         
         # Draw the button
         pygame.draw.rect(win, BLACK, button_rect)  # Draw the button rectangle
         font = pygame.font.SysFont(None, 24)
-        text = font.render("Stop Timer", True, WHITE)
+        text = font.render("Süreyi Durdur", True, WHITE)
         text_rect = text.get_rect(center=button_rect.center)
+        win.blit(text, text_rect)  # Display text on the button
+        
+        # Draw the letter button
+        pygame.draw.rect(win, BLACK, letter_rect)  # Draw the button rectangle
+        font = pygame.font.SysFont(None, 24)
+        text = font.render("Harf Al", True, WHITE)
+        text_rect = text.get_rect(center=letter_rect.center)
         win.blit(text, text_rect)  # Display text on the button
         
         if active:
             pygame.draw.rect(win, BLACK, input_rect, 2)  # Draw the input field rectangle
             text_surface = guess_font.render(guess, True, BLACK)
             win.blit(text_surface, (input_rect.x + 5, input_rect.y + 5))  # Display the guess text
-        
+
         if timer_running:  # Check if the timer is running
             current_time = pygame.time.get_ticks()
             elapsed_time = (current_time - start_time - paused_time) // 1000  # Convert to seconds
             remaining_time = max(time_limit - elapsed_time, 0)
-            timer_text = f"Time left: {remaining_time // 60:02}:{remaining_time % 60:02}"
+            timer_text = f"Kalan Süre: {remaining_time // 60:02}:{remaining_time % 60:02}"
             timer_surface = font.render(timer_text, True, BLACK)
             timer_rect = timer_surface.get_rect(topright=(WIDTH - 20, 50))
             win.blit(timer_surface, timer_rect)
         else:
-            timer_text = "Timer stopped"  # Display a message when the timer is stopped
+            timer_text = "Süre Durduruldu"  # Display a message when the timer is stopped
             timer_surface = font.render(timer_text, True, BLACK)
             timer_rect = timer_surface.get_rect(topright=(WIDTH - 20, 50))
             win.blit(timer_surface, timer_rect)
@@ -126,14 +144,15 @@ async def main():
             if event.type == pygame.QUIT:
                 running = False
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    reveal_random_letter(chosen_word, indexes, guessed_letters)
-                    point_of_word -= 100
                 if active:  # Check if the input field is active
                     if event.key == pygame.K_RETURN:  # If Enter is pressed, check the guess
                         if guess == chosen_word:
                             total_point += point_of_word
-                            chosen_word, word_length, word_description, indexes, point_of_word, guessed_letters = random_word(words)
+                            try:
+                                chosen_word, word_length, word_description, indexes, point_of_word, guessed_letters = random_word(words)
+                            except IndexError:
+                                running = False
+                                break
                             guess = ''
                             active = False
                             timer_running = True
@@ -152,9 +171,11 @@ async def main():
                     if timer_running:
                         timer_running = False  # Stop the timer when the button is clicked
                         paused_start_time = pygame.time.get_ticks()  # Record the time when the timer is paused
-                    
                     # timer_running = False
                     active = True  # Toggle input field focus
+                if letter_rect.collidepoint(event.pos):
+                    reveal_random_letter(chosen_word, indexes, guessed_letters)
+                    point_of_word -= 100
                     
         # Render guessed letters
         font = pygame.font.SysFont(None, 36)
@@ -164,7 +185,16 @@ async def main():
         
         pygame.display.update()  # Update the display
 
-    pygame.quit()  # Quit Pygame
 
+    # After the game loop ends:
+    final_score_font = pygame.font.SysFont(None, 48)
+    final_score_text = final_score_font.render(f'Toplam Skor: {total_point}', True, BLACK)
+    final_score_rect = final_score_text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+    win.fill(WHITE)  # Clear the screen
+    win.blit(final_score_text, final_score_rect)
+    pygame.display.update()
+
+    pygame.time.wait(3000)  # Wait for 3 seconds before quitting (adjust as needed)
+    pygame.quit()  # Quit Pygame
 
 asyncio.run(main())
